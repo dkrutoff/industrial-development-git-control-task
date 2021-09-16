bc59319813f1376516ef35092e8552f7b1aa1116
Dmitry Krutov <d.krutoff@yandex.ru>
2021-09-16 09:48:07 +0300

    Add new line in 'New Feature!'

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world\n!");
-        System.out.println("New Feature!");
+        System.out.println("New Feature!\n");
     }
 }
                  
